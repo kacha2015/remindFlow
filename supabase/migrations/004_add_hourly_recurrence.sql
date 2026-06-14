64dbd20e396b3924dc0fe04fc62f7d3f8b39af92
Marcelo Romitti <mromitti@toolboxtve.com>
2026-06-14 15:45:08 -0300

    change styles and reminders

---
+++ supabase/migrations/004_add_hourly_recurrence.sql
@@ -0,0 +1,5 @@
+-- Add hourly recurrence support
+-- This migration is documentation for the enum/check constraints already handled in app code.
+
+-- If you recreate the schema, make sure recurrence allows:
+-- 'none', 'hourly', 'daily', 'weekly', 'monthly'
